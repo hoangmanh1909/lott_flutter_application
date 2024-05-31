92e840cbfab8420ff81e8dfbcfa7ed24b6727ea4
manhhv <hoangmanh1909@gmail.com>
2024-05-31 17:06:19 +0700

    1

--- android/app/src/main/kotlin/com/example/ketquaxoso/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.lott_flutter_application
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/lott/ketquaxoso/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.lott.ketquaxoso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
